72e048009a8608364e2f59afea93bcbc73801476
Girem <iremashvili21@gmail.com>
2023-02-17 14:44:37 +0400

    07 tempConversion completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function (tempF) {
+  let result = Math.round((((tempF - 32) * 5) / 9) * 10) / 10;
 
-const convertToFahrenheit = function() {
+  return result;
+};
+//Math.round(number * 10) / 10
+const convertToFahrenheit = function (tempC) {
+  let result = Math.round(((tempC * 9) / 5 + 32) * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
