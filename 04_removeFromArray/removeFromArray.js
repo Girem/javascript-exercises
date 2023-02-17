7def7b51ecf1bb1781a4c782d369b0cc508c7f1a
Girem <iremashvili21@gmail.com>
2023-02-17 14:22:25 +0400

    task 04 removeFromArray completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,20 +2,21 @@ const removeFromArray = function (array1, ...args) {
   let result = [];
   //   if (typeof args == `number`) {
   args.forEach((arg) => {
-    if (typeof arg === `number`) {
-      for (let index = 0; index < array1.length; index++) {
-        if (array1[index] === arg) {
-          array1.splice(index, 1);
-          result = array1;
-        }
+    // if (typeof arg === `number`) {
+    for (let index = 0; index < array1.length; index++) {
+      if (array1[index] === arg) {
+        array1.splice(index, 1);
+        result = array1;
+      } else {
+        result = array1;
       }
-    } else {
-      result = array1;
     }
+    // } else {
+    //   result = array1;
+    // }
   });
 
   return result;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
