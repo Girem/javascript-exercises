08c431b4b6f067806f8ab855df607faa927ac113
Girem <iremashvili21@gmail.com>
2023-03-04 22:31:27 +0400

    09_palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let arr1 = Array.from(str.toLowerCase()).filter(
+    (word) => !["!", ",", ".", ` `].includes(word)
+  );
+  let arr2 = arr1.slice().reverse();
 
+  if (arr1.join() === arr2.join()) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
