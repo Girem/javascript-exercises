a52433ab6a7f0dfb57ea31129598ae3c17b1db6e
Girem <iremashvili21@gmail.com>
2023-03-04 21:40:07 +0400

    08 calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  let sum = num1 + num2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  let sub = num1 - num2;
+  return sub;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  array.forEach((element) => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let result = 1;
+  array.forEach((element) => {
+    result *= element;
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num == 0) return 1;
+  else {
+    let result = 1;
+    for (i = 1; i <= num; i++) {
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +46,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
