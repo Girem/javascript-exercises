4bbf76429622e89bf137c1f6962666d1abd85275
Girem <iremashvili21@gmail.com>
2023-03-05 10:53:03 +0400

    10_fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  num = parseInt(num);
+  if (num > 0) {
+    var fibArray = [1, 1];
+    for (var i = 2; i < num; i++) {
+      fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+    }
+    return fibArray.at(num - 1);
+  } else return `OOPS`;
 };
 
 // Do not edit below this line
