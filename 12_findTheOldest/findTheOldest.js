08c186361454a19c978335c200a33c89aadace9c
Girem <iremashvili21@gmail.com>
2023-03-05 16:46:53 +0400

    with help of chatgpt 12_findTheOldest is done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  function getAge(person) {
+    const yearOfDeath = person.yearOfDeath || new Date().getFullYear();
+    return yearOfDeath - person.yearOfBirth;
+  }
+  const currentYear = new Date().getFullYear();
 
+  const oldestPerson = people.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest);
+    const currentAge = getAge(current);
+
+    if (currentAge > oldestAge) {
+      return current;
+    } else {
+      return oldest;
+    }
+  });
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
